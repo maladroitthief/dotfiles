4e7f6b594b3347dbd6a31dd77b7e8f7c02dbb198
Ian Weller <maladroitthief@gmail.com>
2023-07-18 23:14:14 -0400

    fix: cht.sh
    - Fixes the query for languages

--- .config/tmux/tmux-cht.sh
+++ .config/tmux/tmux-cht.sh
@@ -8,9 +8,9 @@ fi
 
 read -p "Enter Query: " query
 
-if grep -qs "$selected" ~/.tmux-cht-languages; then
+if grep -qs "$selected" $config_dir/.tmux-cht-languages; then
     query=`echo $query | tr ' ' '+'`
-    tmux neww bash -c "echo \"curl cht.sh/$selected/$query/\" & curl cht.sh/$selected/$query & while [ : ]; do sleep 1; done"
+    tmux neww bash -c "curl -s cht.sh/$selected/$query | less -r"
 else
     tmux neww bash -c "curl -s cht.sh/$selected~$query | less -r"
 fi
