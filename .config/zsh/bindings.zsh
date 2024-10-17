6128fa0baf3223fef0fb26084e66726aa0f1c9ff
Ian Weller <maladroitthief@gmail.com>
2024-10-17 09:43:00 -0400

    fix: bindings

--- .config/zsh/bindings.zsh
+++ .config/zsh/bindings.zsh
@@ -1,2 +1,9 @@
-bind -x '"\C-f": dir=$(find * -type d | fzf) && cd $dir'
-bind -x '"\C-p": dir=$(find * -type d | fzf) && cd $dir && nvim .'
+find_dir() {
+    dir=$(find * -type d 2>/dev/null | fzf --tmux);
+    cd $dir;
+}
+
+
+zle -N find_dir
+
+bindkey "^f" find_dir
