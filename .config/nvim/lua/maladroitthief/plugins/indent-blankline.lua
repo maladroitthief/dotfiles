4740c97beff3da43d240588c4407b554d22b93c3
Ian Weller <maladroitthief@gmail.com>
2024-10-31 08:41:01 -0400

    fix: Undercurls in tmux

--- .config/nvim/lua/maladroitthief/plugins/indent-blankline.lua
+++ .config/nvim/lua/maladroitthief/plugins/indent-blankline.lua
@@ -1,9 +1,10 @@
 return {
     'lukas-reineke/indent-blankline.nvim',
     main = "ibl",
+    tag = "v3.8.2", -- TODO: Remove tagged version -ian
     opts = {
         indent = {
-            char = '┃',
+            char = "┃",
         },
         scope = {
             show_start = false,
