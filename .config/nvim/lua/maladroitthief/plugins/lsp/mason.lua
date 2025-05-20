2a05e44c5626a138d3a8ad4696b5a5a3d39ff3a3
Ian Weller <maladroitthief@gmail.com>
2025-05-20 08:56:57 -0400

    feat: Golf
    - Adds nvim golf
    - Fixes mason

--- .config/nvim/lua/maladroitthief/plugins/lsp/mason.lua
+++ .config/nvim/lua/maladroitthief/plugins/lsp/mason.lua
@@ -1,30 +1,4 @@
 return {
-    "williamboman/mason.nvim",
-    dependencies = {
-        "williamboman/mason-lspconfig.nvim",
-        "WhoIsSethDaniel/mason-tool-installer.nvim",
-    },
-    config = function()
-        local mason = require("mason")
-        local mason_lspconfig = require("mason-lspconfig")
-
-         mason.setup({
-          ui = {
-            icons = {
-              package_installed = "✓",
-              package_pending = "➜",
-              package_uninstalled = "✗",
-            },
-          },
-        })
-
-        mason_lspconfig.setup({
-      -- list of servers for mason to install
-      ensure_installed = {
-        "html",
-        "cssls",
-        "lua_ls",
-      },
-    })
-    end
+    "mason-org/mason.nvim",
+    opts = {}
 }
