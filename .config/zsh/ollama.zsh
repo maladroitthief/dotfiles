a33764dda54b18ca741e60e469ce3512eeb03382
Ian Weller <maladroitthief@gmail.com>
2025-05-29 09:35:42 -0400

    feat: Ollama api to tailscale

--- .config/zsh/ollama.zsh
+++ .config/zsh/ollama.zsh
@@ -1,2 +1,3 @@
-export OLLAMA_API_BASE=http://127.0.0.1:11434
+# export OLLAMA_API_BASE=http://127.0.0.1:11434
+export OLLAMA_API_BASE=http://orthanc.tailc701bb.ts.net:11434
 export OLLAMA_CONTEXT_LENGTH=8192
