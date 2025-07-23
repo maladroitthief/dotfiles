832a18e9e3dcb8bc27337805b64dfd0e77ee7250
Ian Weller <maladroitthief@gmail.com>
2025-07-23 08:57:33 -0400

    fix: dev plugin breaking nvim

--- .config/nvim/lua/maladroitthief/plugins/dev.lua
+++ .config/nvim/lua/maladroitthief/plugins/dev.lua
@@ -1,9 +1,9 @@
 local M = {}
 
-M.dir = "~/workspace/maladroitthief/jj-minidiff.nvim"
-M.config = function()
-  -- local JJ = require("jj-minidiff")
-  -- M.jj_minidiff = JJ
-end
+-- -- M.dir = "~/workspace/maladroitthief/jj-minidiff.nvim"
+-- M.config = function()
+--   -- local JJ = require("jj-minidiff")
+--   -- M.jj_minidiff = JJ
+-- end
 
 return M
