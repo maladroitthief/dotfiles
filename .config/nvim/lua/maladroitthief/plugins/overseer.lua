102f73c7e1043ad7521ff4bcc2bac7abbd200c1b
Ian Weller <maladroitthief@gmail.com>
2025-09-23 08:25:44 -0400

    feat: Overseer new component
    - adds component to give more details on tasks

--- .config/nvim/lua/maladroitthief/plugins/overseer.lua
+++ .config/nvim/lua/maladroitthief/plugins/overseer.lua
@@ -3,6 +3,7 @@ local overseer_prefix = "<leader>o"
 return {
 	{
 		"stevearc/overseer.nvim",
+		opts = {},
 		config = function()
 			require("overseer").setup({})
 			vim.api.nvim_create_autocmd("BufEnter", {
@@ -12,6 +13,15 @@ return {
 					end
 				end,
 			})
+			local overseer = require("overseer")
+			overseer.add_template_hook({ module = "^make$" }, function(task_defn, util)
+				util.remove_component(task_defn, "on_complete_notify")
+				util.add_component(task_defn, { "myplugin.on_complete_notify_full", max_lines = 40 })
+			end)
+			overseer.add_template_hook({ module = "^shell$" }, function(task_defn, util)
+				util.remove_component(task_defn, "on_complete_notify")
+				util.add_component(task_defn, { "myplugin.on_complete_notify_full", max_lines = 40 })
+			end)
 		end,
 		keys = {
 			{
@@ -24,9 +34,23 @@ return {
 			{
 				overseer_prefix .. "c",
 				function()
-					vim.api.nvim_command("OverseerRunCmd")
+					local overseer = require("overseer")
+					overseer.run_template({
+						name = "shell",
+					})
 				end,
-				desc = "overseer: run command",
+				desc = "overseer: shell",
+			},
+			{
+				overseer_prefix .. "C",
+				function()
+					local overseer = require("overseer")
+					overseer.run_template({
+						name = "shell",
+						cwd = vim.fn.expand("%:p:h"),
+					})
+				end,
+				desc = "overseer: cwd shell",
 			},
 			{
 				overseer_prefix .. "a",
@@ -62,6 +86,29 @@ return {
 				desc = "overseer: restart last command",
 			},
 			{
+				overseer_prefix .. "w",
+				function()
+					local overseer = require("overseer")
+					local tasks = overseer.list_tasks({ recent_first = true })
+					if vim.tbl_isempty(tasks) then
+						vim.notify("No tasks found", vim.log.levels.WARN)
+					else
+						overseer.run_action(tasks[1], "watch")
+					end
+				end,
+				desc = "overseer: watch last command",
+			},
+			{
+				overseer_prefix .. "m",
+				function()
+					local overseer = require("overseer")
+					overseer.run_template({
+						name = "make",
+					})
+				end,
+				desc = "overseer: make",
+			},
+			{
 				overseer_prefix .. "tp",
 				function()
 					local overseer = require("overseer")
@@ -84,29 +131,6 @@ return {
 					task:start()
 				end,
 				desc = "overseer: terraform init",
-			},
-			{
-				overseer_prefix .. "1",
-				function()
-					-- TODO: This should find the buffer if it already exists -ian
-					local overseer = require("overseer")
-
-					vim.cmd("only")
-					local main = vim.api.nvim_get_current_win()
-					local side = vim.api.nvim_open_win(0, true, {
-						split = "left",
-					})
-
-					overseer.run_template({ name = "make" }, function(task)
-						if task then
-							task:add_component({ "restart_on_save", paths = { vim.fn.expand("%:p") } })
-							overseer.run_action(task, "open hsplit")
-						end
-					end)
-
-					vim.api.nvim_set_current_win(main)
-				end,
-				desc = "overseer: build layout",
 			},
 		},
 	},
