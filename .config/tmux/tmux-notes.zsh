834e04df47fd1914f505d8921b1bf8a9c32292f9
Ian Weller <maladroitthief@gmail.com>
2026-04-21 10:29:27 -0400

    feat: We are back to tmux and ghostty

--- .config/tmux/tmux-notes.zsh
+++ .config/tmux/tmux-notes.zsh
@@ -4,17 +4,17 @@ set -o errexit
 set -o nounset
 set -o pipefail
 if [[ "${TRACE-0}" == "1" ]]; then
-    set -o xtrace
+	set -o xtrace
 fi
 
 cd "$(dirname "$0")"
 
 main() {
-    # NOTE: This is for prettier to work
-    # source ~/.config/zsh/nvm.zsh
-    notes_dir="$HOME/workspace/notes.md"
+	# NOTE: This is for prettier to work
+	# source ~/.config/zsh/nvm.zsh
+	notes_dir="$HOME/workspace/notes.md"
 
-    tmux neww -n "notes" nvim $notes_dir
+	tmux neww -n "notes" nvim $notes_dir
 }
 
 main "$@"
