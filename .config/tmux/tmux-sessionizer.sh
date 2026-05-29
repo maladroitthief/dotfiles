a6755f9ffa3e86e79720e0db3126bf782b4dd830
Ian Weller <maladroitthief@gmail.com>
2026-05-29 10:44:08 -0400

    feat: Add defaults to sessionizer

--- .config/tmux/tmux-sessionizer.sh
+++ .config/tmux/tmux-sessionizer.sh
@@ -15,11 +15,25 @@ tmux_running=$(pgrep tmux)
 
 if [[ -z $TMUX ]] && [[ -z $tmux_running ]]; then
 	tmux new-session -s $selected_name -c $selected
+	tmux rename-window -t $selected_name:1 'nvim'
+	tmux new-window -t $selected_name -c $selected -n 'jj'
+	tmux split-window -t $selected_name:2 -c $selected -h
+	tmux send-keys -t $selected_name:2.1 'jj st' Enter
+	tmux send-keys -t $selected_name:2.2 'wjj' Enter
+	tmux select-pane -t $selected_name:2.1
 	exit 0
 fi
 
 if ! tmux has-session -t=$selected_name 2> /dev/null; then
 	tmux new-session -ds $selected_name -c $selected
+	tmux rename-window -t $selected_name:1 'nvim'
+	tmux new-window -t $selected_name -c $selected -n 'jj'
+	tmux split-window -t $selected_name:2 -c $selected -h
+	tmux send-keys -t $selected_name:2.1 'jj st' Enter
+	tmux send-keys -t $selected_name:2.2 'wjj' Enter
+	tmux select-pane -t $selected_name:2.1
+	tmux switch-client -t $selected_name:1
+	exit 0
 fi
 
 tmux switch-client -t $selected_name
